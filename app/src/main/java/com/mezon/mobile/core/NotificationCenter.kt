9649f35f747b9a6730fc0ac521ee372ebdc04f59
Nguyễn Ngọc Anh Tuấn <tuan.nguyenngocanh@ncc.asia>
2026-05-04 21:01:13 +0700

    feat: add clan banner upload and transformation

--- app/src/main/java/com/mezon/mobile/core/NotificationCenter.kt
+++ app/src/main/java/com/mezon/mobile/core/NotificationCenter.kt
@@ -80,6 +80,7 @@ class NotificationCenter(val currentAccount: Int) {
         val channelsDidLoad = nextId()
         val channelAppsDidLoad = nextId()
         val clanInfoUpdated = nextId()
+        val clanBannerCropped = nextId()
         val accountInfoLoaded = nextId()
         val blockedUsersLoaded = nextId()
         val friendsLoaded = nextId()
